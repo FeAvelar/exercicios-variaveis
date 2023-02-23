e062c13ef7a0c04678bfd858b0a826a85e2762f5
Fernando Avelar <fernandoestevaoa@gmail.com>
2023-02-23 00:07:31 -0300

    exercicio variaveis 11

--- exercicios-variaveis11.js
+++ exercicios-variaveis11.js
@@ -1,4 +1,4 @@
-let raio = 3;
 let diametro = 6;
+let raio = diametro / 2;
 let volume = 4 /3 * Math.PI * Math.pow(raio, 3) / 3;
 console.log(`O volume de uma esfera de raio 3 é de aproximadamente ${volume} PI`);
