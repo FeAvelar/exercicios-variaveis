6e523ec952781477d75ce7f2889127afeff982e8
Fernando Avelar <fernandoestevaoa@gmail.com>
2023-02-22 22:05:01 -0300

    exercicio variaveis 05

---
+++ exercicios-variaveis05.js
@@ -0,0 +1,8 @@
+let x1 = 1;
+let x2 = 1;
+let y1 = 1;
+let y2 = 4;
+let ponto_1 = Math.pow(x2 - x1, 2);
+let ponto_2 = Math.pow(y2 - y1, 2);
+let distancia = Math.sqrt(ponto_1 + ponto_2);
+console.log(distancia);
